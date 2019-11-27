8b8bacd8b27a7d5775566196ddefee5f8ebd5775
jjlevy <jjlevy@crimson.ua.edu>
2019-11-26 20:59:35 -0600

    fixed wrong welcome file

--- welcome.php
+++ welcome.php
@@ -1,5 +1,8 @@
-<form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
