b99834758d9ed0a1d95ee6fca88d985487db1bb6
jjlevy <jjlevy@crimson.ua.edu>
2019-12-02 00:14:55 -0600

    style sheet

--- buyer/order_list.php
+++ buyer/order_list.php
@@ -11,7 +11,7 @@
 
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="../../style.css">
+		<link rel="stylesheet" type="text/css" href="../style.css">
 	</head>
 	<body>
 		<h1>Assignments</h1>
